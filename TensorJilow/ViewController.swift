560e15c009bee8a13ed5c3f4e3c702e2d871ae8b
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-10-01 11:34:59 +0900

    initial commit

---
+++ TensorJilow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TensorJilow
+//
+//  Created by 清 貴幸 on 2016/10/01.
+//  Copyright © 2016年 Jirockathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
